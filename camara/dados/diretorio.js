c6c89aa5a620fda5f5a590cae0f3829015351adf
Caroline Morais <cmarquesmorais@byupathway.edu>
2026-09-12 17:26:19 -0400

    Atividade semana02 - Finalizada

--- camara/dados/diretorio.js
+++ camara/dados/diretorio.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function obterDadosMembros() {
     try {
-      const resposta = await fetch("scripts/membros.json");
+      const resposta = await fetch("dados/membros.json");
       if (!resposta.ok) {
         throw new Error(`Erro ao carregar JSON: ${resposta.status}`);
       }
